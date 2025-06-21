54df0add9a321916131faffd14cf6ed9625a9669
gingerknight <ryanrichmond6@gmail.com>
2025-06-20 22:21:26 -0600

    Update:gitignore, Add:ruff toml

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python script.py \"your prompt here\"")
+        print('Usage: python script.py "your prompt here"')
         sys.exit(1)
 
     prompt = sys.argv[1]
@@ -19,15 +20,14 @@ def main():
         print("Error: GEMINI_API_KEY is not set in the environment.")
         sys.exit(1)
 
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=prompt)])
-    ]
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
     print(f"{response.text}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
