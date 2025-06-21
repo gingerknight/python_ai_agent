c6d2737dd59a4915aeccc029948d955692b31e65
gingerknight <ryanrichmond6@gmail.com>
2025-06-20 23:02:04 -0600

    Add:verbose option, Update:swap to argparse, Add:calculator pkg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 from dotenv import load_dotenv
 
 from google import genai
@@ -7,26 +8,35 @@ from google.genai import types
 
 
 def main():
-    if len(sys.argv) < 2:
-        print('Usage: python script.py "your prompt here"')
-        sys.exit(1)
-
-    prompt = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", help="Prompt text to send to LLM model", type=str)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Verbosity: Print user prompt, and user/response tokens in output",
+        required=False,
+        action="store_true",
+    )
+    args = parser.parse_args()
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY not found. Check .env for typos and an API key exists")
 
     if not api_key:
         print("Error: GEMINI_API_KEY is not set in the environment.")
         sys.exit(1)
 
-    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
     print(f"{response.text}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
